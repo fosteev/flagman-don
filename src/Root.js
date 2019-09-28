5a1b28188e5946ce6163a65a3d428d6b10d7ed0f
Your Name <you@example.com>
2019-09-28 22:29:50 +0300

    add ui-kit

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,21 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
 import './App.css';
+import '../node_modules/uikit/dist/css/uikit.min.css';
 
 class Root extends Component {
     render() {
-        return (<div>React + webpack 4 + react router 4 + babel 7</div>);
+        return (
+            <div className="uk-container">
+                <div className="uk-card uk-card-body uk-card-primary">
+                    <h3 className="uk-card-title">Example headline</h3>
+
+                    <button className="uk-button uk-button-default" uk-tooltip="title: Hello World">Hover</button>
+                </div>
+            </div>
+        );
     }
 
 }
